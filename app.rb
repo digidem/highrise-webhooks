de0e495b95a824a10d2c8178d14935603b46818a
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-03-14 12:54:13 -0700

    Move Highrise::Person logic into class method

--- app.rb
+++ app.rb
@@ -20,35 +20,20 @@ Highrise::Base.format = :xml
 # Use an unguessable string for webhook_path for (some) added security.
 post '/webhooks/stripe/' + settings.webhook_path do
   @event = Webhook::Stripe.new(request.body.read)
+  
   case @event.type
   when "charge.succeeded"
-    # Check to see if this customer is already in Highrise (by email lookup)
-    # Highrise::Person.search returns an array, we just want the first record.
-    @donor = Highrise::Person.search(:email => customer.email)[0]
-  
-    # If they are not in Highrise, create a new person in Highrise
-    # http://stackoverflow.com/questions/11902757/getting-head-round-this-gem-api-highrise
-    # https://github.com/37signals/highrise-api/blob/master/sections/people.md
-    if !@donor
-      @donor = Highrise::Person.new(
-        :name => charge.card.name,
-        :contact_data => {
-          :email_addresses => [
-            :email_address => { :address => customer.email }
-          ]
-        }
-      )
-      @donor.save
-    end
+    # Lookup contact in Highrise or create a new contact
+    @donor = Highrise::Person.find_or_create(:email => @event.email, :name => @event.name)
   
     # Create a new Deal in Highrise for the donation, attached to the donor.
     # https://github.com/37signals/highrise-api/blob/master/sections/deals.md
     @donation = Highrise::Deal.new(
-      :name => charge.description,
+      :name => @event.description,
       :party_id => @donor.id,
       :visible_to => "NamedGroup",
       :group_id => settings.highrise_group_id,
-      :price => charge.amount/100,
+      :price => @event.amount/100,
       :currency => "USD",
       :price_type => "fixed",
       :category_id => settings.highrise_donations_category_id
@@ -57,5 +42,30 @@ post '/webhooks/stripe/' + settings.webhook_path do
   
     # Deals are initially created as "Pending", since the charge/donation is already made, this is now "Won"
     @donation.update_status("won")
+  end
+end
+
+class Highrise::Person
+  # Extend Highrise::Person with method to lookup contact via email
+  # and create a new contact if it doesn't exist.
+  def self.find_or_create(attributes = nil)
+    # Check to see if this customer is already in Highrise (by email lookup)
+    # Highrise::Person.search returns an array, we just want the first record.
+    @donor = self.search(:email => attributes[:email]).first
+    # If they are not in Highrise, create a new person in Highrise
+    # http://stackoverflow.com/questions/11902757/getting-head-round-this-gem-api-highrise
+    # https://github.com/37signals/highrise-api/blob/master/sections/people.md
+    if !@donor
+      @donor = self.new(
+        :name => attributes[:name],
+        :contact_data => {
+          :email_addresses => [
+            :email_address => { :address => attributes[:email] }
+          ]
+        }
+      )
+      @donor.save
+    end
+    @donor
   end
 end
