d10c77148ee8e24117a1286a7257b55265bf3ccb
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-03-14 12:26:18 -0700

    Add webhook gem, remove unnecessary code.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "thin"
 gem "highrise"
-gem "stripe"
+gem "webhook", :git => 'git@github.com:digidem/webhook-gem.git'
 gem "json"
 
 group :development do
--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'highrise'
-require 'stripe'
+require 'webhook'
 require 'json'
 
 #Set secret keys from environment variables
@@ -12,65 +12,50 @@ set :highrise_donations_category_id, 2730561
 
 set :protection, except: :ip_spoofing
 
-Stripe.api_key = settings.stripe_secret_key
+Webhook::Stripe.api_key = settings.stripe_secret_key
 Highrise::Base.site = 'https://ddem.highrisehq.com'
 Highrise::Base.user = settings.highrise_api_token
 Highrise::Base.format = :xml
 
 # Use an unguessable string for webhook_path for (some) added security.
 post '/webhooks/' + settings.webhook_path do
+  @event = Webhook::Stripe.new(request.body.read)
+  case @event.type
+  when "charge.succeeded"
+    # Check to see if this customer is already in Highrise (by email lookup)
+    # Highrise::Person.search returns an array, we just want the first record.
+    @donor = Highrise::Person.search(:email => customer.email)[0]
   
-  # Stripe webhooks post JSON. Parse the JSON into event_json
-  # https://stripe.com/docs/webhooks
-  event_json = JSON.parse(request.body.read)
+    # If they are not in Highrise, create a new person in Highrise
+    # http://stackoverflow.com/questions/11902757/getting-head-round-this-gem-api-highrise
+    # https://github.com/37signals/highrise-api/blob/master/sections/people.md
+    if !@donor
+      @donor = Highrise::Person.new(
+        :name => charge.card.name,
+        :contact_data => {
+          :email_addresses => [
+            :email_address => { :address => customer.email }
+          ]
+        }
+      )
+      @donor.save
+    end
   
-  # For more security, retrieve the actual event from Stripe to make sure it really exists.
-  event = Stripe::Event.retrieve(event_json['id'])
-  
-  # Only respond to "charge.succeeded" events
-  halt 200 unless event.type == "charge.succeeded"
-  
-  # The charge object https://stripe.com/docs/api?lang=ruby#charges
-  charge = event.data.object
-  
-  # Retrieve details about the Stripe customer that has been charged
-  # https://stripe.com/docs/api?lang=ruby#retrieve_customer
-  customer = Stripe::Customer.retrieve(charge.customer)
-
-  # Check to see if this customer is already in Highrise (by email lookup)
-  # Highrise::Person.search returns an array, we just want the first record.
-  @donor = Highrise::Person.search(:email => customer.email)[0]
-  
-  # If they are not in Highrise, create a new person in Highrise
-  # http://stackoverflow.com/questions/11902757/getting-head-round-this-gem-api-highrise
-  # https://github.com/37signals/highrise-api/blob/master/sections/people.md
-  if !@donor
-    @donor = Highrise::Person.new(
-      :name => charge.card.name,
-      :contact_data => {
-        :email_addresses => [
-          :email_address => { :address => customer.email }
-        ]
-      }
+    # Create a new Deal in Highrise for the donation, attached to the donor.
+    # https://github.com/37signals/highrise-api/blob/master/sections/deals.md
+    @donation = Highrise::Deal.new(
+      :name => charge.description,
+      :party_id => @donor.id,
+      :visible_to => "NamedGroup",
+      :group_id => settings.highrise_group_id,
+      :price => charge.amount/100,
+      :currency => "USD",
+      :price_type => "fixed",
+      :category_id => settings.highrise_donations_category_id
     )
-    @donor.save
+    @donation.save
+  
+    # Deals are initially created as "Pending", since the charge/donation is already made, this is now "Won"
+    @donation.update_status("won")
   end
-  
-  # Create a new Deal in Highrise for the donation, attached to the donor.
-  # https://github.com/37signals/highrise-api/blob/master/sections/deals.md
-  @donation = Highrise::Deal.new(
-    :name => charge.description,
-    :party_id => @donor.id,
-    :visible_to => "NamedGroup",
-    :group_id => settings.highrise_group_id,
-    :price => charge.amount/100,
-    :currency => "USD",
-    :price_type => "fixed",
-    :category_id => settings.highrise_donations_category_id
-  )
-  @donation.save
-  
-  # Deals are initially created as "Pending", since the charge/donation is already made, this is now "Won"
-  @donation.update_status("won")
-  
 end
